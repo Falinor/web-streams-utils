f266778ff75bb2f41d8a70cca61e4768dbc07f06
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-04-23 09:24:50 +0200

    ci: skip commitlint validation on release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
+const automaticCommitPattern = /^chore\(release\):.*\[skip ci]/
+
 export default {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  ignores: [commit => automaticCommitPattern.test(commit)]
 }
